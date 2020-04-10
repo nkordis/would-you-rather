fca23c239adc8ead0ad694e2dfea87a704a321a5
nkordis <nikoskordis@gmail.com>
2020-04-10 12:02:01 +0200

    feat: add helper to format question's timestamp

---
+++ src/utils/helpers.js
@@ -0,0 +1,9 @@
+/**
+ * Formats the timestamp of a question
+ * @param {} timestamp
+ */
+export function formatDate(timestamp) {
+  const d = new Date(timestamp);
+  const time = d.toLocaleTimeString("en-US");
+  return time.substr(0, 5) + time.slice(-2) + " | " + d.toLocaleDateString();
+}
