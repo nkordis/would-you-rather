77ecacc46a78595f8b2f92adc9f620d362716528
nkordis <nikoskordis@gmail.com>
2020-04-10 18:51:01 +0200

    feat: add sign in page

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -3,16 +3,44 @@ import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 
 class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.dispatch(handleInitialData(this.state.tempSelected));
+  };
+
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({ tempSelected: e.target.value });
+  };
+
   render() {
     return (
       <div className="SignIn">
-        <button
-          onClick={() =>
-            this.props.dispatch(handleInitialData("tylermcginnis"))
-          }
-        >
-          Sign In
-        </button>
+        <h3>Welcome to the Would You Rather App!</h3>
+        <p>Please sign in to continue</p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <p style={{ color: "#c1c1c1" }}>Sign in</p>
+            <select
+              value={this.state.tempSelected}
+              defaultValue="defaultV"
+              onChange={this.handleChange}
+            >
+              <option disabled value="defaultV">
+                -- Select User --
+              </option>
+              <option value="sarahedot">Sarah Edo</option>
+              <option value="tylermcginnis">Tyler McGinnis</option>
+              <option value="johndoe">John Doe</option>
+            </select>
+          </label>
+          <input type="submit" value="Sign in" />
+        </form>
       </div>
     );
   }
