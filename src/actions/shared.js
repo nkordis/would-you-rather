81577fd9f79ef9fd7b7f18c22b2f167abca79778
nkordis <nikoskordis@gmail.com>
2020-04-10 20:42:21 +0200

    feat: add actions to users and questions intial data

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,11 +1,13 @@
 import { getInitialData } from "../utils/api";
-import { setAuthedUser } from "../actions/authedUser";
-
-//const AUTHED_ID = "tylermcginnis";
+import { setAuthedUser } from "./authedUser";
+import { receiveUsers } from "./users";
+import { receiveQuestions } from "./questions";
 
 export function handleInitialData(AUTHED_ID) {
   return (dispatch) => {
-    return getInitialData().then(() => {
+    return getInitialData().then(({ users, tweets }) => {
+      dispatch(receiveUsers(users));
+      dispatch(receiveQuestions(tweets));
       dispatch(setAuthedUser(AUTHED_ID));
     });
   };
