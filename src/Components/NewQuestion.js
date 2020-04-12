a487a657d579212ea96aa47220fa7581d8ee2d20
nkordis <nikoskordis@gmail.com>
2020-04-12 17:49:10 +0200

    feat: add form to create new questions

--- src/Components/NewQuestion.js
+++ src/Components/NewQuestion.js
@@ -1,8 +1,40 @@
 import React, { Component } from "react";
 
 class NewQuestion extends Component {
+  handleVoteSumbit(e) {
+    alert("it works!");
+    e.preventDefault();
+  }
+
   render() {
-    return <div>New Question</div>;
+    return (
+      <div>
+        <h1>Create New Question</h1>
+        <p>Complete the question:</p>
+        <p>
+          <strong>Wou you rather ...</strong>
+        </p>
+        <form onSubmit={this.handleVoteSumbit}>
+          <textarea
+            placeholder="Enter Option One Text Here"
+            /*value=""*/
+            onChange={this.handleChange}
+            className="textarea"
+            maxLength={50}
+          />
+          <textarea
+            placeholder="Enter Option Two Text Here"
+            /*value=""*/
+            onChange={this.handleChange}
+            className="textarea"
+            maxLength={50}
+          />
+          <button className="btn" type="submit" /*disabled={text === ""}*/>
+            Submit
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
